1357f64dcf4fd492a374c59633c34c19df7a03fd
a.k.a gustipang <93385061+gustipangestu@users.noreply.github.com>
2024-01-29 10:05:58 +0700

    session 4 added

---
+++ session 04/main.js
@@ -0,0 +1,53 @@
+import * as THREE from 'three';
+
+// create Scene 
+
+const scene = new THREE.Scene(); 
+
+scene.background = new THREE.Color('skyblue'); 
+
+// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); 
+
+ 
+
+// create Camera 
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); 
+
+camera.position.x = -1; 
+
+camera.position.z = 13; 
+
+camera.position.y = 3; 
+
+ 
+
+// Renderer  
+
+const renderer = new THREE.WebGLRenderer(); 
+
+renderer.setSize(window.innerWidth, window.innerHeight); 
+
+document.body.appendChild(renderer.domElement); 
+
+ 
+
+// plane geometry 
+
+const geometry = new THREE.PlaneGeometry( 5, 5 ); 
+
+const material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} ); 
+
+const plane = new THREE.Mesh( geometry, material ); 
+
+scene.add(plane); 
+
+function animate() {
+    requestAnimationFrame(animate);
+    cube2.rotation.x += 0.01;
+    // cube.rotation.y += 0.001;
+    cube2.rotation.z += 0.01;
+    renderer.render(scene, camera);
+}
+
+animate();
