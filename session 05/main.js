b8fec8588a46cab7aaad8f02c7846f6ae47802bd
a.k.a gustipang <93385061+gustipangestu@users.noreply.github.com>
2024-01-30 14:47:42 +0700

    aset

--- session 05/main.js
+++ session 05/main.js
@@ -32,70 +32,86 @@ document.body.appendChild(renderer.domElement);
 // scene.add(points);
 
 
-const createGhostTexture = () => {
-    const canvas = document.createElement('canvas');
-    canvas.width = 70;
-    canvas.height = 70;
-    const ctx = canvas.getContext('2d');
-    // the body
-    ctx.translate(-81, -84);
-    ctx.fillStyle = 'orange';
-    ctx.beginPath();
-    ctx.moveTo(83, 116);
-    ctx.lineTo(83, 90);
-    ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
-    // some code removed for clarity
-    ctx.fill();
-    // the eyes
-    ctx.fillStyle = 'white';
-    ctx.beginPath();
-    ctx.moveTo(91, 96);
-    ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
-    ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
-    // some code removed for clarity
-    ctx.fill();
-    // the pupils
-    ctx.fillStyle = 'blue';
-    ctx.beginPath();
-    ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
-    ctx.fill();
-    ctx.beginPath();
-    ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
-    ctx.fill();
+// const createGhostTexture = () => {
+//     const canvas = document.createElement('canvas');
+//     canvas.width = 70;
+//     canvas.height = 70;
+//     const ctx = canvas.getContext('2d');
+//     // the body
+//     ctx.translate(-81, -84);
+//     ctx.fillStyle = 'orange';
+//     ctx.beginPath();
+//     ctx.moveTo(83, 116);
+//     ctx.lineTo(83, 90);
+//     ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
+//     // some code removed for clarity
+//     ctx.fill();
+//     // the eyes
+//     ctx.fillStyle = 'white';
+//     ctx.beginPath();
+//     ctx.moveTo(91, 96);
+//     ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
+//     ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
+//     // some code removed for clarity
+//     ctx.fill();
+//     // the pupils
+//     ctx.fillStyle = 'blue';
+//     ctx.beginPath();
+//     ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
+//     ctx.fill();
+//     ctx.beginPath();
+//     ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
+//     ctx.fill();
+//     const texture = new THREE.Texture(canvas);
+//     texture.needsUpdate = true;
+//     return texture;
+// }
+
+// const createPoints = () => {
+//     const points = [];
+//     const range = 15;
+//     for (let i = 0; i < 1500000; i++) {
+//         let particle = new THREE.Vector3(
+//             Math.random() * range - range / 2,
+//             Math.random() * range - range / 2,
+//             Math.random() * range - range / 2
+//         )
+//         points.push(particle);
+//     }
+//     const colors = new Float32Array(points.length * 3);
+//     points.forEach((e, i) => {
+//         const c = new THREE.Color(Math.random() * 0xff00ff);
+//         colors[i * 3] = c.r;
+//         colors[i * 3 + 1] = c.g;
+//         colors[i * 3 + 2] = c.b;
+//     })
+//     const geom = new THREE.BufferGeometry().setFromPoints(points);
+//     geom.setAttribute('color', new THREE.BufferAttribute(colors, 3, true));
+//     return geom;
+// }
+// const material = new THREE.PointsMaterial({
+//     size: 0.1,
+//     vertexColors: true, color: 0xffffff, map:
+//         createGhostTexture()
+// });
+// const points = new THREE.Points(createPoints(), material);
+// scene.add(points);
+
+
+// draw a line
+const drawLine = () => {
+    var c = document.createElement("myCanvas");
+    canvas.width = 700;
+    canvas.height = 700;
+    var ctx = c.getContext("2d");
+    ctx.moveTo(0, 0);
+    ctx.lineTo(700, 100);
+    ctx.stroke();
     const texture = new THREE.Texture(canvas);
     texture.needsUpdate = true;
     return texture;
-}
 
-const createPoints = () => {
-    const points = [];
-    const range = 15;
-    for (let i = 0; i < 1500000; i++) {
-        let particle = new THREE.Vector3(
-            Math.random() * range - range / 2,
-            Math.random() * range - range / 2,
-            Math.random() * range - range / 2
-        )
-        points.push(particle);
-    }
-    const colors = new Float32Array(points.length * 3);
-    points.forEach((e, i) => {
-        const c = new THREE.Color(Math.random() * 0xff00ff);
-        colors[i * 3] = c.r;
-        colors[i * 3 + 1] = c.g;
-        colors[i * 3 + 2] = c.b;
-    })
-    const geom = new THREE.BufferGeometry().setFromPoints(points);
-    geom.setAttribute('color', new THREE.BufferAttribute(colors, 3, true));
-    return geom;
 }
-const material = new THREE.PointsMaterial({
-    size: 0.1,
-    vertexColors: true, color: 0xffffff, map:
-        createGhostTexture()
-});
-const points = new THREE.Points(createPoints(), material);
-scene.add(points);
 
 camera.position.z = 5;
 
