ec49093337c9680faab8e2c227bc29f8e664ce21
a.k.a gustipang <93385061+gustipangestu@users.noreply.github.com>
2024-02-12 16:55:53 +0700

    orbitcontroll

--- session 06/next_three/src/app/sesi6/page.tsx
+++ session 06/next_three/src/app/sesi6/page.tsx
@@ -1,41 +1,43 @@
 "use client"
 
+import { OrbitControls } from '@react-three/drei';
 import { Canvas, MeshBasicMaterialProps, RingGeometryProps, ThreeElements, useFrame, extend } from '@react-three/fiber'
 import React, { useState, useEffect, useRef } from 'react';
 import { BoxGeometry, ConeGeometry, Mesh } from 'three'
 extend({ ConeGeometry })
 
-function RotationMesh(){
+function RotationMesh() {
     const meshRef = useRef<Mesh>(null!)
-    
 
-    useFrame( ({clock}) => {
+
+    useFrame(({ clock }) => {
         const a = clock.getElapsedTime();
         meshRef.current.rotation.x = a;
         meshRef.current.rotation.y = a;
     })
-    
+
     return (
         <>
-        <mesh ref={meshRef}>
-          <coneGeometry args={[2,2,3]} />
-          <meshPhongMaterial color="green" />
-        </mesh>
+            <mesh ref={meshRef}>
+                <coneGeometry args={[2, 2, 9]} />
+                <meshLambertMaterial color="green" />
+            </mesh>
         </>
-      );
-    
+    );
+
 }
 
 export default function sesi6() {
-    
+
     return (
         <>
             <div className='w-dvh h-dvh'>
-            <Canvas>
-        <RotationMesh />
-        <ambientLight intensity={3.9} />
-        <directionalLight />
-      </Canvas>
+                <Canvas>
+                    <RotationMesh />
+                    <ambientLight intensity={3.9} />
+                    <directionalLight />
+                    <OrbitControls/>
+                </Canvas>
             </div>
         </>
     )
