59b0d97dce71694158153392ab3498707198492a
a.k.a gustipang <93385061+gustipangestu@users.noreply.github.com>
2024-02-15 16:05:01 +0700

    hover

--- session 06/next_three/src/app/sesi6/page.tsx
+++ session 06/next_three/src/app/sesi6/page.tsx
@@ -3,25 +3,51 @@
 import { OrbitControls } from '@react-three/drei';
 import { Canvas, MeshBasicMaterialProps, RingGeometryProps, ThreeElements, useFrame, extend } from '@react-three/fiber'
 import React, { useState, useEffect, useRef } from 'react';
-import { BoxGeometry, ConeGeometry, Mesh } from 'three'
+import THREE, { BoxGeometry, ConeGeometry, Mesh } from 'three'
+import { color } from 'three/examples/jsm/nodes/Nodes.js';
 extend({ ConeGeometry })
 
-function RotationMesh() {
-    const meshRef = useRef<Mesh>(null!)
+function colorr(hv: boolean){
+    var color;
+    if(hv==true){
+        color = "pink"
+    }
+    else{
+        color = "blue"
+    }
+    return color;
+}
 
+function RotationMesh() {
+    
+    const meshRef = useRef<Mesh>(null!)
+    const [hovered, setHover] = useState(false);
+    
 
     useFrame(({ clock }) => {
         const a = clock.getElapsedTime();
-        meshRef.current.rotation.x = a;
-        meshRef.current.rotation.y = a;
+        meshRef.current.rotation.x += 0.01;
+        meshRef.current.rotation.y += 0.01;
     })
 
+       
+    
     return (
+        console.log(hovered),
         <>
-            <mesh ref={meshRef}>
+         
+            <mesh receiveShadow  ref={meshRef} 
+           onPointerOver={(event) => setHover(true)}
+           onPointerOut={(event) => setHover(false)}
+            >
                 <coneGeometry args={[2, 2, 9]} />
-                <meshLambertMaterial color="green" />
+                {/* <meshPhysicalMaterial color={'blue'} /> */}
+                <meshStandardMaterial color={hovered ? "skyblue" : "orange" } 
+                roughness={0} metalness={0.8} envMapIntensity={2} 
+                />
+                
             </mesh>
+            
         </>
     );
 
@@ -32,8 +58,10 @@ export default function sesi6() {
     return (
         <>
             <div className='w-dvh h-dvh'>
-                <Canvas>
+                <Canvas style={{ background: "hotpink" }}>
+                
                     <RotationMesh />
+                    
                     <ambientLight intensity={3.9} />
                     <directionalLight />
                     <OrbitControls/>
@@ -43,3 +71,10 @@ export default function sesi6() {
     )
 
 }
+
+function useHover() {
+    const [hovered, hover] = useState(false)
+    return [hovered, { onPointerOver: () => hover(true), onPointerOut: () => hover(false) }]
+  }
+
+  
